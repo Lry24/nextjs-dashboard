61c6f29aa4d94fc357322f749e5daa27d332917f
leroypn <panaleroy@gmail.com>
2026-05-10 11:54:59 +0000

    finish Next.Js course in the website of DOcumentation

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+
+  {
+    rules: {
+      '@next/next/no-html-link-for-pages': 'off',
+    },
+  },
+
+  globalIgnores([
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+]);
+
+export default eslintConfig;
